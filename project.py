cdcc88170e362e8631c4cddca9903bd35552f3b0
Kartik <kartikdps.kg@gmail.com>
2019-07-03 20:33:26 +0530

    first commit

---
+++ project.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+driver = webdriver.Chrome("/Users/kartikgupta/Downloads/chromedriver")
+
+username = "techwidkay@gmail.com"
+password = "qerkiK-1fuhbo-rufpum"
+url = "https://www.gmail.com"
+import datetime
+
+
+driver.get(url)
+driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys(username)
+driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys(Keys.ENTER)
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(
+    password
+)
+driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(
+    Keys.ENTER
+)
+time.sleep(10)
+i = 1
+while i < 5:
+    driver.find_element_by_xpath(
+        '//*[@id=":4"]/div/div[1]/div[1]/div/div/div[5]/div/div'
+    ).click()
+    mail1 = driver.find_element_by_xpath('//*[@class="zA zE"]').get_attribute("id")
+    time.sleep(10)
+    mail2 = driver.find_element_by_xpath('//*[@class="zA zE"]').get_attribute("id")
+    if mail1 == mail2:
+        continue
+    else:
+        current_time = datetime.datetime.now()
+        print("New mail : {}".format(current_time))
+        continue
